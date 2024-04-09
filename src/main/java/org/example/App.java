504ae8d4c8546d6cdfda5db9845618cc99fbc1a8
tommy-lean <k-tomilin99@mail.ru>
2024-04-09 21:54:41 +0500

    test change

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,5 +6,8 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
